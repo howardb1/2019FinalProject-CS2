972ede73f991d218976212e5170d6c0fc09ccdf6
brianhoward <brianhoward@10.220.26.45>
2019-12-06 19:39:18 -0500

    unresolvable error
    Good Luck

--- FinalProject2019CS2/src/Game/GalagaGUI.java
+++ FinalProject2019CS2/src/Game/GalagaGUI.java
@@ -1,5 +1,8 @@
 package Game;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
@@ -10,19 +13,34 @@ import javafx.stage.Stage;
 
 public class GalagaGUI extends Application {
 
+	Button button;
+	
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			launch(args);
 	}
 
 	@Override
-	public void start(Stage arg0) throws Exception {
+	public void start(Stage stage) throws Exception {
 		// TODO Auto-generated method stub
 		
 		
+		stage.setTitle("Galaga");
+		button = new Button("Start Here");
 		
 		
+		StackPane Backdrop = new StackPane();
+		Backdrop.getChildren().add(button);
 		
+		
+		Scene scene = new Scene (Backdrop, 400,400);
+		
+		stage.setScene(scene);
+		
+		stage.show();
 		
 		
 		
